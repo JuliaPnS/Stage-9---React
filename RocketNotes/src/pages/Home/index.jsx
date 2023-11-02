b7c8eafb182716b1a36dc62932a0cd717637f502
julia <ju.pennas@gmail.com>
2023-11-02 16:09:01 -0300

    create Home

---
+++ RocketNotes/src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+import { FiPlus } from 'react-icons/fi';
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+
+import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
+
+export function Home() {
+    return (
+        <Container>
+            <Brand>
+            <h1>RocketNotes</h1>
+            </Brand>
+            <Header/>
+
+            <Menu>
+                <li><ButtonText title="Todos"/></li>
+                <li><ButtonText title="React"/></li>
+                <li><ButtonText title="Node"/></li>
+
+            </Menu>
+
+            <Search>
+
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewNote>
+                <FiPlus/>
+                    Criar Nota
+            </NewNote>
+        </Container>
+    );
+}
