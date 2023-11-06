9cea3ed76c9859bf10401ee37f7fe93b4169901c
julia <ju.pennas@gmail.com>
2023-11-05 21:46:47 -0300

    finishing styles pages

--- RocketNotes/src/pages/New/index.jsx
+++ RocketNotes/src/pages/New/index.jsx
@@ -1,5 +1,9 @@
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { TextArea } from '../../components/TextArea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 
 import { Container, Form } from './styles';
@@ -15,6 +19,21 @@ export function New() {
                         <a href='/'>Voltar</a>
                     </header>
                     <Input placeholder="Título" />
+                    <TextArea placeholder="Observações"/>
+
+                    <Section title="Links Úteis">
+                        <NoteItem value="https://rocketseat.com.br"/>
+                        <NoteItem isNew placeholder="Novo Link"/>
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            <NoteItem value="React"/>
+                            <NoteItem isNew placeholder="Nova Tag"/>
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar" />
 
                 </Form>
             </main>
