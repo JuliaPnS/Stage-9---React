e1593484787a0b2bad530d5aa40b37f49b115dc7
julia <ju.pennas@gmail.com>
2023-11-05 22:08:00 -0300

    installed react-dom routes + creating routes

--- RocketNotes/src/components/ButtonText/styles.js
+++ RocketNotes/src/components/ButtonText/styles.js
@@ -3,11 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.button`
 
     background: none;
-<<<<<<< HEAD:RocketNotes/src/components/ButtonText/styles.js
     color: ${({ theme, $isactive }) => $isactive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100};
-=======
-    color: ${({ theme, $isactive }) =>  $isactive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100};
->>>>>>> b7c8eafb182716b1a36dc62932a0cd717637f502:RocketNotes/src/components/ButtonText/style.js
 
     border: none;
     font-size: 16px;
